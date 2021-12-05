13ff0e3709ae93063a029220bff68024c557030b
adityacaturputra <adityacaturputra25@gmail.com>
2021-12-05 11:44:33 +0700

    Membuat API GET - List Image

--- routes/media.js
+++ routes/media.js
@@ -5,6 +5,21 @@ const base64Img = require('base64-img');
 
 const { Media } = require('../models');
 
+router.get('/', async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ['id', 'image']
+  });
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m
+  })
+
+  return res.json({
+    status: 'success',
+    data: mappedMedia
+  })
+});
+
 router.post('/', (req, res) => {
   const image = req.body.image;
   if (!isBase64(image, { mimeRequired: true })){
@@ -18,7 +33,7 @@ router.post('/', (req, res) => {
 
     const filename = filepath.split("\\").pop().split("/").pop();
 
-    const media = await Media.create({ image: `image/${filename}` })
+    const media = await Media.create({ image: `images/${filename}` })
 
     return res.status(200).json({
       status: 'success',
